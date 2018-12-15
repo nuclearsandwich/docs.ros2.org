5a6eb9f7acefe29d65c1a2f68488434ee4a348c7
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-12-14 22:09:35 -0800

    Regenerate crystal docs with macros (maybe).

--- crystal/api/rcutils/search/typedefs_2.js
+++ crystal/api/rcutils/search/typedefs_2.js
@@ -6,7 +6,7 @@ var searchData=
   ['rcutils_5ferror_5fstate_5ft',['rcutils_error_state_t',['../error__handling_8h.html#a54602d3d4b7a5b19ba2be0baeef1c59e',1,'error_handling.h']]],
   ['rcutils_5ferror_5fstring_5ft',['rcutils_error_string_t',['../error__handling_8h.html#acbbc1d485ee2979a9b4b868c8e202e33',1,'error_handling.h']]],
   ['rcutils_5flog_5flocation_5ft',['rcutils_log_location_t',['../logging_8h.html#a70b5d9c9429189fe125ccdac5006c4e8',1,'logging.h']]],
-  ['rcutils_5flogging_5foutput_5fhandler_5ft',['rcutils_logging_output_handler_t',['../logging_8h.html#af6ba3a2b969c395ee5b41a544d47f8e5',1,'logging.h']]],
+  ['rcutils_5flogging_5foutput_5fhandler_5ft',['rcutils_logging_output_handler_t',['../logging_8h.html#ad114a8385fda34e7050cc327065e66d6',1,'logging.h']]],
   ['rcutils_5fret_5ft',['rcutils_ret_t',['../rcutils__ret_8h.html#a016de12b840689bdc6c96cab97b08c21',1,'rcutils_ret.h']]],
   ['rcutils_5fstring_5farray_5ft',['rcutils_string_array_t',['../string__array_8h.html#ae97717b3511839370e33e5c4fcc50424',1,'string_array.h']]],
   ['rcutils_5fstring_5fmap_5ft',['rcutils_string_map_t',['../string__map_8h.html#aa176b76a3dd5dc62dd3222574b26de71',1,'string_map.h']]],
